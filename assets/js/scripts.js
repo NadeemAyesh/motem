497a148c60c697e0cedbc5481754284dd494fcaf
n.ayesh <n.ayesh@newline.ps>
2020-05-05 18:08:40 +0300

    update all of content amd testimonials

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -58,6 +58,74 @@ $(document).ready(function () {
             $(this).find('.need-text p').slideUp();
         });
     });
+
+    let paginationLeftPos = "20px";
+    let paginationOpacity = 0;
+    let checkPaginationClick = 0;
+
+    $(".pagination-page-number").click(function () {
+        $(".pagination-page-number").removeClass("active");
+        $(this).addClass("active");
+        paginationLeftPos = $(this).prop("offsetLeft") + "px";
+        paginationOpacity = 1;
+        checkPaginationClick = 1;
+
+        $(".pagination-hover-overlay").css({
+            left: paginationLeftPos,
+            backgroundColor: "#02B3FA",
+            opacity: paginationOpacity
+        });
+
+        $(this).css({
+            color: "#fff"
+        });
+
+    });
+
+    $(".pagination-page-number").hover(
+        function () {
+            paginationOpacity = 1;
+            $(".pagination-hover-overlay").css({
+                backgroundColor: "#02B3FA",
+                left: $(this).prop("offsetLeft") + "px",
+                opacity: paginationOpacity
+            });
+
+
+            $(".pagination-page-number.active").css({
+                color: "#333d45"
+            });
+
+
+            $(this).css({
+                color: "#fff"
+            });
+
+        },
+        function () {
+            if (checkPaginationClick) {
+                paginationOpacity = 1;
+            } else {
+                paginationOpacity = 0;
+            }
+
+            $(".pagination-hover-overlay").css({
+                backgroundColor: "#02B3FA",
+                opacity: paginationOpacity,
+                left: paginationLeftPos
+            });
+
+
+            $(this).css({
+                color: "#333d45"
+            });
+
+
+            $(".pagination-page-number.active").css({
+                color: "#fff"
+            });
+
+        });
     
     // inialize animate WOW Js
     new WOW().init();
